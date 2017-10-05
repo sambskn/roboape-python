356bf185993bef39af9b5ba4444ac43dc5864fe5
sambskn <sambaskin@ou.edu>
2017-10-05 13:17:33 -0500

    think i had null checking wrong learning this python thing as we go

--- botlogic.py
+++ botlogic.py
@@ -11,7 +11,7 @@ def getResponse(data):
 		for keyword in template['keywords']:
 			if keyword in message.lower():
 				#check if we are doing something special
-				if template['special'] is None:
+				if template['special'] == None:
 					potentialResponses = template['responses']
 					if random.uniform(0, 1)<template['frequency'][0]:
 						return random.choice(potentialResponses)
