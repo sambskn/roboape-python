c8f9cc5127db8c730ea612902cb6a26ca355f218
sambskn <sambaskin@ou.edu>
2017-10-06 09:33:35 -0500

    hopefully fixed issues with retireval formatting

--- retrieval.py
+++ retrieval.py
@@ -1,22 +1,21 @@
 import os
 import json
 import requests
-import datetime
+
 
 URL = 'https://api.groupme.com/v3'
 TOKEN = os.getenv('GROUPME_TOKEN')
 BOT_ID = os.getenv('GROUPME_BOT_ID')
 
+def get(response):
+	"""Get the resposne portion of the JSON object"""
+	return response.json()['response']
 
-def getGroupID():
-    """Returns the group id asscoicated with the bot"""
-    bots = get(requests.get(URL + '/bots' + TOKEN, params=params))
-	groupid = 0
-	for bot in bots:
-		if bot['bot_id'] == BOT_ID:
-			groupid = bot['group_id']
-	return groupid
-
+def getGroupMembers():
+	"""get list of members in the group the bot is asscoiated with"""
+	group_id = getGroupID()
+	group = get(requests.get(URL + '/groups/:' + group_id + TOKEN))
+	return group['members']
 
 def getUserIDForUser(name):
     """
@@ -31,23 +30,39 @@ def getUserIDForUser(name):
     return userID
 
 
+def getGroupID():
+	"""returns the group ID associated with the bot"""
+	bots = get(requests.get(URL + '/bots' + TOKEN))
+	groupid = 0
+	for bot in bots:
+		if bot['bot_id'] == BOT_ID:
+			groupid = bot['group_id']
+			return groupid
+	return 0
+
+
 def getNumFavorited(msg):
 	"""Counts the number of favorites the mssage received."""
 	num_favorited = msg['favorited_by']
 	return len(num_favorited)
 
-
 def getAllMessages(user_ID=None):
-    group_id = getGroupID()
-	group = get(requests.get(URL + '/groups/' + group_id + TOKEN)
-    totalCount=group['messages']['count']
-    before_id=group['messages']['last_message_id']  # might need to add a -1
-    output=[]
-    params={'before_id': before_id, 'limit': 100}
-	count=0
-	msgs=get(requests.get(URL + '/groups/:' + group_id + \
-	         '/messages' + TOKEN, params=params))
-    while count < totalCount:
+	"""
+	this will return a list of msg dicts, 
+	with the optional check to only get ones matching the given user_id
+	"""
+	group_id = getGroupID()
+	group = get(requests.get(URL + '/groups/:' + group_id + TOKEN))
+	totalCount = group['messages']['count']
+	before_id = group['messages']['last_message_id'] #might need a '-1' here
+	output = []
+	params = {
+		'before_id': before_id,
+		'limit': 100
+	}
+	count = 0
+	msgs = get(requests.get(URL + '/groups/:' + group_id + '/messages' + TOKEN, params=params))
+	while count < totalCount:
 		for msg in msgs:
 			if user_ID is None:
 				output.append(msg)
@@ -55,9 +70,10 @@ def getAllMessages(user_ID=None):
 				if msg['user_id'] == user_ID:
 					output.append(msg)
 			count += 1
-		before_id=before_id - 100
-
-		params={'before_id': before_id, 'limit': 100}
-		msgs=get(requests.get(URL + '/groups/:' + group_id + \
-		         '/messages' + TOKEN, params=params))
+		before_id = before_id - 100
+		params = {
+			'before_id': before_id,
+			'limit': 100
+		}
+		msgs = get(requests.get(URL + '/groups/:' + group_id + '/messages' + TOKEN, params=params))
 	return output
