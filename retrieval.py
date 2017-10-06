ffc0cc209b2a4f02e4c07aac30e24d463f250546
sambskn <sambaskin@ou.edu>
2017-10-06 11:36:37 -0500

    testing the api calls in retrieval

--- retrieval.py
+++ retrieval.py
@@ -9,6 +9,7 @@ BOT_ID = os.getenv('GROUPME_BOT_ID')
 
 def get(response):
 	"""Get the resposne portion of the JSON object"""
+	print(response.json()['response'])
 	return response.json()['response']
 
 def getGroupMembers():
