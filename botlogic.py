f7eb73130fbf48ec9a3ad5350736d9a920dd95e2
sambskn <sambaskin@ou.edu>
2017-10-28 16:41:00 -0500

    tried to fix the formatting issue woof

--- botlogic.py
+++ botlogic.py
@@ -1,26 +1,26 @@
 import os
 import json
 import random
+from collections import Counter
 from watson_developer_cloud import PersonalityInsightsV3
 import retrieval
-from collections import Counter
 
 def getResponse(data):
-	#see groupme API for details about data JSON object
+	#see groupme API for details about dat JSON object
 	with open('chatBank.json') as json_data:
-		chatBank = json.load(json_data)
+		chatBank =json.load(json_data)
 	message = data['text']
 	for template in chatBank['templates']:
 		for keyword in template['keywords']:
 			if keyword in message.lower():
-				#check if we are doing something special
+				#check if it has a special tag
 				if template['special'] is None:
 					potentialResponses = template['responses']
 					if random.uniform(0, 1)<template['frequency'][0]:
 						return random.choice(potentialResponses)
 				else:
 					if template['special'] == 'personality':
-						#TODO Make a watson call here
+						#TODO Make a Watson Call
 						print('watson process started')
 						msgs = retrieval.getAllMessages(data['user_id'])
 						prepared = prepareForWatson(msgs)
@@ -28,16 +28,17 @@ def getResponse(data):
 						return prepared
 						watsonresults = getWatsonPersonalityData(prepared)
 						print(watsonresults)
-						return 'ooh yeah lets see them results'
+						return 'oohyeah lets see results'
 					return template['special']
 
 def prepareForWatson(msgs):
 	"""
-	preparing suitable data to be sent to the Watson API
-	reccomended amount for maxium precison is 3000 words
+	prepare suitable data to be sent to Watson for analysis
+	reccomended amount for precision is ~3000 words
+	probably not all fromt he same source but oh well
 	"""
 	MAX_WORDS = 3000
-	output= []
+	output = []
 	wordCount = 0
 	wordsInMsg = 0
 	for msg in msgs:
@@ -47,17 +48,13 @@ def prepareForWatson(msgs):
 			wordCount += wordsInMsg
 		else:
 			break
-		
 	output = ''.join(output)
 	return output
 
 def getWatsonPersonalityData(input):
 	psycheval = PersonalityInsightsV3(
-		version='2016-10-20',
-		username=os.getenv('WATSON_PERSON_USERNAME'),
-		password=os.getenv('WATSON_PERSON_PASSWORD')
+		version = '2016-10-20',
+		username = os.getenv('WATSON_PERSON_USERNAME'),
+		password = os.getenv('WATSON_PERSON_PASSWORD') 
 	)
 	return psycheval.profile(input)
-
-
-	
